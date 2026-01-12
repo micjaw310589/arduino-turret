b8db2d1b200568d5b09809747a9dd6a832c1fed3
Mateusz Rymoniak <mr310718@student.polsl.pl>
2026-01-12 15:50:35 +0100

    Added BT capability

--- turret-pico/turret-pico.cpp
+++ turret-pico/turret-pico.cpp
@@ -1,10 +1,19 @@
 #include <stdio.h>
 #include <random>
+#include <string.h>
 #include "pico/stdlib.h"
 #include "hardware/pwm.h"
 #include "hardware/gpio.h"
 #include "hardware/clocks.h"
 #include "hardware/structs/rosc.h"
+#include "pico/cyw43_arch.h"
+#include "pico/btstack_cyw43.h"
+#include "btstack.h"
+
+// Bluetooth SPP
+static uint16_t rfcomm_channel_id = 0;
+static uint8_t spp_service_buffer[150];
+static bool bt_connected = false;
 
 // Definicje Pinów
 constexpr int ECHO_PIN = 6;
@@ -85,9 +94,18 @@ void write_distance_to_buffer(const ServoPosition &scanner_servo, float distance
 void write_distance_to_map(const ServoPosition &scanner_servo, float distance_cm);
 AimPosition get_position_to_target();
 void shoot();
+<<<<<<< Updated upstream
 void random_deg_pos(ServoPosition &scanner_servo);
 void random_scan_next_step(Servo &ver_servo, Servo &hor_servo, ServoPosition &scanner_servo);
 void linear_scan_next_step(Servo &ver_servo, Servo &hor_servo, ServoPosition &scanner_servo);
+=======
+void random_deg_pos(int &ver_pos_deg, int &hor_pos_deg);
+void random_scan_next_step(Servo &ver_servo, Servo &hor_servo, int &ver_pos_deg, int &hor_pos_deg);
+void linear_scan_next_step(Servo &ver_servo, Servo &hor_servo, int &ver_pos_deg, int &hor_pos_deg, 
+                      bool &horizontalDirection, bool &verticalDirection);
+void bt_packet_handler(uint8_t packet_type, uint16_t channel, uint8_t *packet, uint16_t size);
+void bt_send_measurement(char axis, int angle, float distance);
+>>>>>>> Stashed changes
 
 // Globalne obiekty serw
 Servo servo_scanner_hor;
@@ -104,6 +122,33 @@ ServoPosition gun_servo_pos = {DEFAULT_GUN_VER_ANGLE, DEFAULT_GUN_HOR_ANGLE};
 void setup() {
     // Inicjalizacja Serial (USB)
     stdio_init_all();
+
+    // Inicjalizacja Bluetooth
+    if (cyw43_arch_init()) {
+        printf("Failed to initialize WiFi/BT module\n");
+        return;
+    }
+
+    // Inicjalizacja BTstack dla Bluetooth Classic
+    l2cap_init();
+    rfcomm_init();
+    rfcomm_register_service(bt_packet_handler, RFCOMM_SERVER_CHANNEL_1, 0xffff);
+
+    // Konfiguracja SPP (Serial Port Profile)
+    sdp_init();
+    memset(spp_service_buffer, 0, sizeof(spp_service_buffer));
+    spp_create_sdp_record(spp_service_buffer, 0x10001, RFCOMM_SERVER_CHANNEL_1, "Turret SPP");
+    sdp_register_service(spp_service_buffer);
+
+    // Konfiguracja Bluetooth jako widoczne urządzenie
+    gap_discoverable_control(1);
+    gap_ssp_set_io_capability(SSP_IO_CAPABILITY_DISPLAY_YES_NO);
+    gap_set_local_name("Pico-Turret");
+
+    // Start HCI
+    hci_power_control(HCI_POWER_ON);
+
+    printf("Bluetooth initialized. Device name: Pico-Turret\n");
 
     // Inicjalizacja rng
     seed_random_from_rosc();
@@ -152,11 +197,22 @@ int main() {
     setup();
 
     while (true) {
+        // Przetwarzanie zdarzeń Bluetooth
+        btstack_run_loop_execute_once();
+
         float distance_cm = read_distance_cm();
         printf("Zmierzona odległość: %.2f cm\n", distance_cm);
 
         write_distance_to_buffer(scanner_servo_pos, distance_cm);
         // write_distance_to_map(scanner_servo_pos, distance_cm);
+
+        // Wysyłanie pomiarów przez Bluetooth
+        if (bt_connected) {
+            // Wysyłanie pozycji poziomej (H)
+            bt_send_measurement('H', scanner_servo_pos.HOR, distance_cm);
+            // Wysyłanie pozycji pionowej (V)
+            bt_send_measurement('V', scanner_servo_pos.VER, distance_cm);
+        }
 
         AimPosition target_pos = get_position_to_target();
 
@@ -436,5 +492,90 @@ void linear_scan_next_step(Servo &ver_servo, Servo &hor_servo, ServoPosition &sc
         }
     }
 
+<<<<<<< Updated upstream
     set_position(ver_servo, hor_servo, scanner_servo);
+=======
+    ver_servo.write(ver_pos_deg);
+    sleep_ms(15);
+    hor_servo.write(hor_pos_deg);
+    sleep_ms(15);
+}
+
+// Obsługa zdarzeń Bluetooth
+void bt_packet_handler(uint8_t packet_type, uint16_t channel, uint8_t *packet, uint16_t size) {
+    UNUSED(channel);
+    UNUSED(size);
+
+    bd_addr_t event_addr;
+    uint8_t rfcomm_channel_nr;
+    uint16_t mtu;
+
+    switch (packet_type) {
+        case HCI_EVENT_PACKET:
+            switch (hci_event_packet_get_type(packet)) {
+                case HCI_EVENT_PIN_CODE_REQUEST:
+                    printf("Pin code request - using '0000'\n");
+                    hci_event_pin_code_request_get_bd_addr(packet, event_addr);
+                    gap_pin_code_response(event_addr, "0000");
+                    break;
+
+                case HCI_EVENT_USER_CONFIRMATION_REQUEST:
+                    printf("SSP User Confirmation Request\n");
+                    break;
+
+                case RFCOMM_EVENT_INCOMING_CONNECTION:
+                    rfcomm_event_incoming_connection_get_bd_addr(packet, event_addr);
+                    rfcomm_channel_nr = rfcomm_event_incoming_connection_get_server_channel(packet);
+                    rfcomm_channel_id = rfcomm_event_incoming_connection_get_rfcomm_cid(packet);
+                    printf("RFCOMM channel %u requested for %s\n", rfcomm_channel_nr, bd_addr_to_str(event_addr));
+                    rfcomm_accept_connection(rfcomm_channel_id);
+                    break;
+
+                case RFCOMM_EVENT_CHANNEL_OPENED:
+                    if (rfcomm_event_channel_opened_get_status(packet)) {
+                        printf("RFCOMM channel open failed, status 0x%02x\n", rfcomm_event_channel_opened_get_status(packet));
+                        bt_connected = false;
+                    } else {
+                        rfcomm_channel_id = rfcomm_event_channel_opened_get_rfcomm_cid(packet);
+                        mtu = rfcomm_event_channel_opened_get_max_frame_size(packet);
+                        printf("RFCOMM channel open succeeded. Channel ID %u, max frame size %u\n", rfcomm_channel_id, mtu);
+                        bt_connected = true;
+                    }
+                    break;
+
+                case RFCOMM_EVENT_CHANNEL_CLOSED:
+                    printf("RFCOMM channel closed\n");
+                    rfcomm_channel_id = 0;
+                    bt_connected = false;
+                    break;
+
+                default:
+                    break;
+            }
+            break;
+
+        case RFCOMM_DATA_PACKET:
+            // Obsługa otrzymanych danych (jeśli potrzebna)
+            printf("Received data: %.*s\n", size, packet);
+            break;
+
+        default:
+            break;
+    }
+}
+
+// Wysyłanie pomiarów przez Bluetooth w formacie "H,kąt,odległość" lub "V,kąt,odległość"
+void bt_send_measurement(char axis, int angle, float distance) {
+    if (!bt_connected || rfcomm_channel_id == 0) {
+        return;
+    }
+
+    char buffer[64];
+    snprintf(buffer, sizeof(buffer), "%c,%d,%.2f\n", axis, angle, distance);
+    
+    int err = rfcomm_send(rfcomm_channel_id, (uint8_t*)buffer, strlen(buffer));
+    if (err) {
+        printf("Error sending data via Bluetooth: %d\n", err);
+    }
+>>>>>>> Stashed changes
 }
